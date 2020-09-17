8c73d39990c43afbea2474717217d8497ccb4152
Johann-Trausta <johannt19@ru.is>
2020-09-17 14:59:13 +0000

    Gerðum algorithma

---
+++ TileTraveller.py
@@ -0,0 +1,5 @@
+# Algorithm:
+# 1. Byrjum á að setja upp staðsetninguna á x og y og inputið þar sem þú setur inn áttina 
+# 2. Setjum síðan upp fjögur föll sem taka inn inputin um áttina og bæta við eða draga frá 
+# 3. Gerum föll sem segja hvar veggirnir eru og hvaða áttir eru mögulegar
+
